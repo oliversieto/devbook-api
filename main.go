fb48c04475365663f063a4fe638c9b08e0c403e5
Renato Oliveira <oliversieto@gmail.com>
2022-03-10 20:52:26 -0400

    feat: create env vars

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"devbook-api/src/config"
 	"devbook-api/src/router"
 	"fmt"
 	"log"
@@ -8,9 +9,10 @@ import (
 )
 
 func main() {
+	config.Load()
 	router := router.Generate()
 
 	fmt.Println("Listening on port 5000")
 
-	log.Fatal(http.ListenAndServe(":5000", router))
+	log.Fatal(http.ListenAndServe(config.ApiPort, router))
 }
---
+++ src/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	DataSourceName = ""
+	ApiPort        = ""
+)
+
+func Load() {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	ApiPort = os.Getenv("API_PORT")
+
+	ApiPort = ":5000"
+
+	DataSourceName = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_USER"))
+
+}
