e4832d0e0a2d682b8dc90dd9b40b114612f91f94
Renato Oliveira <oliversieto@gmail.com>
2022-03-10 20:09:05 -0400

    feat: integrating routes and router

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"devbook-api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	router := router.Generate()
+
+	fmt.Println("Listening on port 5000")
+
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
