14fc021c135616a0c98e8b21d0dff44818bae989
Renato Oliveira <oliversieto@gmail.com>
2022-03-10 20:08:04 -0400

    feat: created users controller

---
+++ src/controllers/users/users.go
@@ -0,0 +1,23 @@
+package users
+
+import "net/http"
+
+func GetAll(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetOne(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+
+}
