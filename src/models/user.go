308cef4866fbcac2ee55229b091d89ae67722ad5
Renato Oliveira <oliversieto@gmail.com>
2022-03-12 15:46:10 -0400

    fix: updated created at to create at and updated at to update at

--- src/models/user.go
+++ src/models/user.go
@@ -7,13 +7,13 @@ import (
 )
 
 type User struct {
-	ID        uint64    `json:"id,omitempty"`
-	Name      string    `json:"name,omitempty"`
-	Nick      string    `json:"nick,omitempty"`
-	Email     string    `json:"email,omitempty"`
-	Phrase    string    `json:"phrase,omitempty"`
-	CreatedAt time.Time `json:"createdAt,omitempty"`
-	UpdatedAt time.Time `json:"updatedAt,omitempty"`
+	ID       uint64    `json:"id,omitempty"`
+	Name     string    `json:"name,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Phrase   string    `json:"phrase,omitempty"`
+	CreateAt time.Time `json:"createdAt,omitempty"`
+	UpdateAt time.Time `json:"updatedAt,omitempty"`
 }
 
 func (user *User) Prepare() error {
