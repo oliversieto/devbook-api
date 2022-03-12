26e3686a659281327a78937400460bea2d66cf89
Renato Oliveira <oliversieto@gmail.com>
2022-03-12 11:40:31 -0400

    feat: created validate func to validate fields. created format func to remove white spaces from words created Prepare func to call validate and format

--- src/models/user.go
+++ src/models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type User struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -10,4 +14,40 @@ type User struct {
 	Phrase    string    `json:"phrase,omitempty"`
 	CreatedAt time.Time `json:"createdAt,omitempty"`
 	UpdatedAt time.Time `json:"updatedAt,omitempty"`
+}
+
+func (user *User) Prepare() error {
+	user.format()
+
+	if err := user.validate(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (user *User) validate() error {
+	if user.Name == "" {
+		return errors.New("o campo nome é obrigatório")
+	}
+
+	if user.Email == "" {
+		return errors.New("o campo e-mail é obrigatório")
+	}
+
+	if user.Nick == "" {
+		return errors.New("o campo nick é obrigatório")
+	}
+
+	if user.Phrase == "" {
+		return errors.New("o campo phrase é obrigatório")
+	}
+
+	return nil
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Email = strings.TrimSpace(user.Email)
+	user.Nick = strings.TrimSpace(user.Nick)
 }
