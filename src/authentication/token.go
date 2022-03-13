475d9ad4c11bd2d7f97467fc02ffdf60b86d3f91
Renato Oliveira <oliversieto@gmail.com>
2022-03-13 18:03:15 -0400

    feat: add token validator

--- src/authentication/token.go
+++ src/authentication/token.go
@@ -2,6 +2,10 @@ package authentication
 
 import (
 	"devbook-api/src/config"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -17,4 +21,37 @@ func TokenGenerator(userID uint64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS512, permissions)
 
 	return token.SignedString(config.SecretKey)
+}
+
+func TokenValidator(r *http.Request) error {
+	tokenString := tokenExtractor(r)
+	token, err := jwt.Parse(tokenString, getVerificationKey)
+
+	if err != nil {
+		return err
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return nil
+	}
+
+	return errors.New("token inválido")
+}
+
+func tokenExtractor(r *http.Request) string {
+	authorization := r.Header.Get("Authorization")
+	token := strings.Split(authorization, " ")
+	if len(token) == 2 {
+		return token[1]
+	}
+
+	return ""
+}
+
+func getVerificationKey(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("método de assinatura inesperado! %v", token.Header["alg"])
+	}
+
+	return config.SecretKey, nil
 }
